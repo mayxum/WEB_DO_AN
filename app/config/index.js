2b20e9a9785d14455089c284b508c0fa995b3bf5
anhtan2223 <anhtan3332@gmail.com>
2023-11-10 09:22:36 +0700

    Ad Script InitSer

--- app/config/index.js
+++ app/config/index.js
@@ -1,5 +1,20 @@
 module.exports = {
     app : {
         PORT : 3000
+    } ,
+    db : {
+        url : "mongodb://127.0.0.1:27017" ,
+        dbName : "QuanLyBanHang" ,
+        collection : 
+        {
+            role    :   "BangChucVu" ,
+            account :   "BangNguoiDung" ,
+            sanpham :   "ThongTinSanPham" ,
+            hoadon  :   "HoaDon" ,
+            chitiet :   "ChiTietHoaDon" ,
+            giohang :   "GioHang" ,
+            truyvan :   "BangTruyVan"
+        }
+
     }
 }
--- server.js
+++ server.js
@@ -1,6 +1,48 @@
 const app = require('./app.js')
-const config = require('./app/config').app
+const config = require('./app/config')
+const Mongo = require('./app/utils/mongo.utils')
+const Service = require('./app/services/mongo.service')
+const InitData = require('./SchemaData')
 
-app.listen(config.PORT,()=>{
-    console.log('Server Listen in Port' , config.PORT)
-})
+async function startServer()
+{
+    try {
+        await Mongo.connect(config.db.url)
+        console.log("Connect To MongoDB Success");
+        const Port = config.app.port
+        app.listen(Port , ()=>{console.log(`Now Server Run On ${Port}`);})
+    } catch (error) {
+        console.log('Connect To DB has Error', error)
+    }
+}
+
+async function InitServer()
+{
+    try {
+        await Mongo.connect(config.db.url)
+        console.log("Connect To MongoDB Success");
+        const collection = new Service()
+        
+        await collection.role.deleteMany({})
+        await collection.account.deleteMany({})
+        await collection.sanpham.deleteMany({})
+        await collection.hoadon.deleteMany({})
+        await collection.chitiet.deleteMany({})
+        await collection.giohang.deleteMany({})
+        await collection.truyvan.deleteMany({})
+
+
+        await collection.role.insertMany(InitData.role)
+        await collection.account.insertMany(InitData.account)
+        await collection.sanpham.insertMany(InitData.sanpham)
+        await collection.hoadon.insertMany(InitData.hoadon)
+        await collection.giohang.insertMany(InitData.giohang)
+        await collection.truyvan.insertMany(InitData.truyvan)
+       
+    } catch (error) {
+        console.log("Error When Init With MongoDB" , error)
+    }
+}
+
+InitServer()
+startServer()
