fda0d5bf62f679568e15c0a618fe27ce67840def
anhtan2223 <anhtan3332@gmail.com>
2023-11-10 08:44:38 +0700

    add ErrorAPI

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const router = require('./app/routes/home.route')
+const ErrorAPI =  require('./app/ErrorAPI')
 
 const app = express()
 
@@ -9,6 +10,18 @@ app.use(express.json())
 
 app.get("",(req,res)=>{
     res.json({message : "Welcome To My Shop "})
+})
+
+app.use((req,res,next)=>{
+    return next(new ErrorAPI(404,"Page Not Found"))
+})
+
+//Catch Error
+app.use((err , req ,res , next ) => {
+    //Xu Ly Loi O Day 
+        return res.status(err.status || 500).json({
+            message : err.message || "Internal Server Error"
+        })
 })
 
 app.use("/api" , router)
---
+++ app/ErrorAPI.js
@@ -0,0 +1,11 @@
+class ErrorAPI extends Error
+{
+    constructor(status,message)
+    {
+        super()
+        this.status = status
+        this.message = message
+    }
+}
+
+module.exports = ErrorAPI
